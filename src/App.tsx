7081ba5ede7512427f7580604e24af7710fe1de2
Dmitriy <fstpain@gmail.com>
2024-06-15 20:21:57 +0300

    durost'

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export const wait = (time: number) => {
+    return new Promise((resolve, reject) => {
+         setTimeout(() => {
+             resolve(true)
+        }, time)
+    })
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    async function run() {
+
+        await wait(1000)
+        console.log(1)
+        await wait(1000)
+        console.log(2)
+        await wait(1000)
+        console.log(3)
+    }
+
+    run()
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
--- src/state.ts
+++ src/state.ts
@@ -1,42 +1,14 @@
-export type ICar = {
-    model: string,
-    year: number,
-    on: boolean,
-    turnOn: () => void,
-    rename: (model: string) => void
+export const wait = (time: number) =>{
+    return setTimeout(()=>{
+
+    }, time)
+}
+console.log('12121')
+async function run(){
+
+    await wait(1000)
+    await wait(1000)
+    await wait(1000)
 }
 
-
-let car: ICar = {
-    model: 'Reno Stepway',
-    year: 2016,
-    on: false,
-    turnOn() {
-        this.on = true;
-    },
-    rename(model) {
-        this.model = model;
-    }
-}
-
-type erere= {
-    addCar: (car:string)=>void
-    logAllCarsNames: ()=>void
-    getAllCars: ()=> string[]
-}
-let createGarage = ():erere  => {
-    let _cars:string[] = [];
-
-    return {
-        addCar(car) {
-            _cars.push(car);
-        },
-        logAllCarsNames() {
-            console.log('Cars in the garage: ');
-            _cars.forEach((c: any) => console.log(c.model));
-        },
-        getAllCars() {
-            return _cars;
-        }
-    }
-}
+run()
